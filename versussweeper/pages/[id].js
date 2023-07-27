2b5beb24d998f70ecf633da1cd5129e8d3e3e405
Akil Selvan Rajendra Janarthanan <arajendr@ucsd.edu>
2023-07-27 13:57:10 -0700

    stupid sutpid

--- versussweeper/pages/[id].js
+++ versussweeper/pages/[id].js
@@ -4,7 +4,7 @@ import ChooseName from "../components/ChooseName";
 import GameFull from "../components/GameFull";
 import GameNotFound from "../components/GameNotFound";
 import Lobby from "../components/Lobby";
-import Minesweeper from "../components/Minesweeper";
+import Minesweeper from "../components/minesweeper";
 import io from "socket.io-client";
 
 export const GameContext = createContext();
