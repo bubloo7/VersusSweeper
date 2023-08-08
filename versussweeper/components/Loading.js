d5840279bdefc2b1cd081b335e5a57a97d29862c
Yash Patki <yashom.patki@gmail.com>
2023-08-08 12:06:17 -0700

    Loading component

--- versussweeper/components/Loading.js
+++ versussweeper/components/Loading.js
@@ -1,9 +1,19 @@
+import React from "react";
+import { LoadingOutlined } from "@ant-design/icons";
+
 export default function Loading() {
-    return (
-        <>
-            <div>Loading...</div>
-        </>
-    );
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh"
+      }}
+    >
+      <LoadingOutlined
+        style={{ fontSize: "50px", color: "var(--darker-gray)" }}
+      />
+    </div>
+  );
 }
-
-
