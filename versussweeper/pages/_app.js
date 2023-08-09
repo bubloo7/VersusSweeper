31c6f69a236183806433ea25ceeee3f28ecbb3e4
Yash Patki <yashom.patki@gmail.com>
2023-08-08 19:29:19 -0700

    New favicon

--- versussweeper/pages/_app.js
+++ versussweeper/pages/_app.js
@@ -18,8 +18,8 @@ export default function App({ Component, pageProps }) {
                 ></meta>
                 <meta name="author" content="Versus Sweeper"></meta>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-                {/* <link rel="icon" href="/favicon.ico" />
-                <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
+                <link rel="icon" href="/bomb-hit.ico" />
+                {/* <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
                 <link rel="manifest" href="/manifest.json"></link> */}
                 <meta name="theme-color" content="#f4f4f4"></meta>
             </Head>
